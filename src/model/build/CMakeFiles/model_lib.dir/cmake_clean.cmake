1a6870aadfa4d415698c1863524822ca4c31a2fd
Jarod Wille <jwille@princeton.edu>
2024-04-11 22:48:33 -0400

    libtorch and the model are finally workinggit add .git add .

---
+++ src/model/build/CMakeFiles/model_lib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/model_lib.dir/model.cc.o"
+  "CMakeFiles/model_lib.dir/model.cc.o.d"
+  "libmodel_lib.pdb"
+  "libmodel_lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/model_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
